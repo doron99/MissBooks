61ed68ed1a1398bbb87a64515f1568e89f3057b8
Your Name <maromdo@gmail.com>
2025-08-31 09:30:30 +0300

    improve home and aboutUs pages, added svg icons and flex-system grid infrastructure for easy manage

--- pages/AboutUs.jsx
+++ pages/AboutUs.jsx
@@ -3,22 +3,34 @@ const { useState, useEffect } = React
 export function AboutUs() {
     return (
         <section className="home">
-            <h2>AboutUs</h2>
-            <section>
-            <h2>Our Story</h2>
-            <p>Founded with a passion for storytelling and community, our bookstore has been a cozy corner for book lovers since day one. What started as a small local shop has grown into a vibrant space where readers of all ages come to discover, connect, and get inspired.</p>
+            
+            <section className="page-header-container w-90">
+                <div style={{display:'flex',alignItems:'center'}}>
+                    <h1>About Us</h1>
+                    <img 
+                    src="assets/svgs/users.svg" 
+                    alt="Tag Icon" 
+                    className="icon-color-clr2"
+                    style={{width:'40px',height:'40px',marginLeft:'5px'}} />
+                </div>
+                 <h2>Our Story</h2>
+                 <p>Founded with a passion for storytelling and community, our bookstore has been a cozy corner for book lovers since day one. What started as a small local shop has grown into a vibrant space where readers of all ages come to discover, connect, and get inspired.</p>
+            </section>
+            <div className="row w-90" style={{margin:'auto'}}>
+                <section className="col-md-6">
+                    <h2>What We Believe</h2>
+                    <p>We believe books have the power to educate, entertain, and transform lives. Our mission is to make reading accessible, enjoyable, and meaningful for everyone — from curious kids to lifelong learners.</p>
+                </section>
+
+                <section className="col-md-6">
+                    <h2>Why Choose Us</h2>
+                    <p>Whether you're searching for the latest bestseller, a rare classic, or just a quiet place to explore, we’re here to help. With personalized recommendations, friendly service, and a carefully curated collection, we’re more than just a bookstore — we’re your reading partner.</p>
+                </section>
+
+               
+            </div> 
         </section>
 
-        <section>
-            <h2>What We Believe</h2>
-            <p>We believe books have the power to educate, entertain, and transform lives. Our mission is to make reading accessible, enjoyable, and meaningful for everyone — from curious kids to lifelong learners.</p>
-        </section>
-
-        <section>
-            <h2>Why Choose Us</h2>
-            <p>Whether you're searching for the latest bestseller, a rare classic, or just a quiet place to explore, we’re here to help. With personalized recommendations, friendly service, and a carefully curated collection, we’re more than just a bookstore — we’re your reading partner.</p>
-        </section>
-       </section>
        
     )
 }
--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -3,27 +3,36 @@ const { useState, useEffect } = React
 export function HomePage() {
     return (
     <section className="home">
-        <h2>Miss Books</h2>
-         <section>
+        
+        <section className="page-header-container w-90">
+            <div style={{display:'flex',alignItems:'center'}}>
+                    <h1>Miss Books</h1>
+
+                    <img 
+                    src="assets/svgs/home.svg" 
+                    alt="Tag Icon" 
+                    className="icon-color-clr2"
+                    style={{width:'40px',height:'40px',marginLeft:'5px'}} />
+                </div>
             <h2>Explore Our Collection</h2>
             <p>Browse thousands of titles across all genres including fiction, non-fiction, mystery, romance, fantasy, science, history, and more. Whether you're a casual reader or a lifelong book lover, there's something here for everyone.</p>
         </section>
+        <div className="row w-90" style={{margin:'auto'}}>
+            <section className="col-md-4">
+                <h2>New Arrivals</h2>
+                <p>Stay up to date with the latest releases from top authors around the world. Our collection is updated weekly with fresh titles, so you'll never run out of options.</p>
+            </section>
 
-        <section>
-            <h2>New Arrivals</h2>
-            <p>Stay up to date with the latest releases from top authors around the world. Our collection is updated weekly with fresh titles, so you'll never run out of options.</p>
-        </section>
+            <section className="col-md-4">
+                <h2>Staff Picks</h2>
+                <p>Not sure where to start? Check out our staff recommendations — carefully selected reads we think you'll love. From hidden gems to literary favorites, we've got you covered.</p>
+            </section>
 
-        <section>
-            <h2>Staff Picks</h2>
-            <p>Not sure where to start? Check out our staff recommendations — carefully selected reads we think you'll love. From hidden gems to literary favorites, we've got you covered.</p>
-        </section>
-
-        <section>
-            <h2>Visit Us In-Store or Shop Online</h2>
-            <p>Prefer to flip through pages in person? Visit our cozy store to experience books the old-fashioned way. Or enjoy fast, convenient online shopping with home delivery or in-store pickup.</p>
-        </section>
-                
+            <section className="col-md-4">
+                <h2>Visit Us In-Store or Shop Online</h2>
+                <p>Prefer to flip through pages in person? Visit our cozy store to experience books the old-fashioned way. Or enjoy fast, convenient online shopping with home delivery or in-store pickup.</p>
+            </section>
+        </div> 
     </section>
     )
 }
