4fba59d8b40fb1d5bc3f7e2ed66b15a21f217f71
doron99 <maromdo@gmail.com>
2025-09-15 09:01:31 +0300

    clean the code in dashboard

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -10,30 +10,29 @@ export function Dashboard() {
         loadBooks();
     },[])
     function loadBooks() {
-                bookService.query(bookService.getDefaultFilter())
-                    .then(books => {
-                        setBooks(books);
-                       
-                        const groupBy = books.reduce((accumulator, element) => {
-                            const groupKey = element.categories && element.categories[0] ? element.categories[0] : 'undefined';
-                            // Initialize the count for the groupKey if it doesn't exist
-                            accumulator[groupKey] = (accumulator[groupKey] || 0) + 1;
-                            return accumulator; // Return the accumulator for the next iteration
-                        }, {});
-                        const _arrDashboardData = [];
-                        Object.entries(groupBy).map(([key, value]) => {
-                            _arrDashboardData.push({title:key,value:value})
-                        })
-                        setArrDashboardData(_arrDashboardData);
-
-                        console.log(groupBy);
-                        const styles = `display:grid;`
-                    })
-                    .catch(err => {
-                        console.error('err:', err)
-                        showErrorMsg('Cannot load books')
-                    })
-            }
+        bookService.query(bookService.getDefaultFilter())
+            .then(books => {
+                setBooks(books);
+                
+                const groupBy = books.reduce((accumulator, element) => {
+                    const groupKey = element.categories && element.categories[0] ? element.categories[0] : 'undefined';
+                    accumulator[groupKey] = (accumulator[groupKey] || 0) + 1;
+                    return accumulator;
+                }, {});
+                const data = Object.keys(groupBy)
+                    .map(cat =>
+                    ({
+                        title: cat,
+                        value: Math.round((groupBy[cat] / books.length) * 100)
+                    }))
+                
+                setArrDashboardData(data);
+            })
+            .catch(err => {
+                console.error('err:', err)
+                showErrorMsg('Cannot load books')
+            })
+    }
  
 
     return (
